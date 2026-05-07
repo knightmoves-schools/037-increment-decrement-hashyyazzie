9578b31d53272dd0b0e0c6f18ef92a25335d04d7
Craig <craigyazzie143@icloud.com>
2026-05-07 12:58:39 -0600

    Update index.js
    h

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let counter = 0;
 let prefixIncrement = ++counter;
-let counter = 0;
+counter = 0;
 let postfixIncrement = counter++;
-let counter = 0;
+counter = 0;
 let prefixDecrement = --counter;
-let counter = 0;
+counter = 0;
 let postfixDecrement = counter--;
 
 document.getElementById('result').innerHTML = counter;
